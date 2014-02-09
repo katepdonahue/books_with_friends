057c8bf21d50a9668096e0183cadd1bf303495bc
Kate <katepdonahue@gmail.com>
2014-02-09 17:59:51 -0500

    Add associations to reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ActiveRecord::Base
   has_many :users, :through => :book_users
   belongs_to :author
   has_many :reviews
+  
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ActiveRecord::Base
+  belongs_to :book
+  belongs_to :user
+  belongs_to :author, :through => :book
 end
