fcc280ed514a7b19608b5609d03e2818a7101c6f
Kate <katepdonahue@gmail.com>
2014-02-09 18:02:10 -0500

    Add associations for reviews

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  has_many :books
+  has_many :reviews, :through => :books
+  has_many :book_users, :through => :books
+  has_many :users, :through => :book_users
 end
