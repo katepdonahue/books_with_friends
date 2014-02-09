4fc70042538d7efaa32b1e020ff938990170319d
Kate <katepdonahue@gmail.com>
2014-02-09 17:53:05 -0500

    Add associations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :book_users
+  has_many :books, :through => :book_users
+  has_many :authors, :through => :books
+  has_many :reviews
+
 end
