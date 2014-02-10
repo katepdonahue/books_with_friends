b880fa0fd86327e25a9d8f371afd19434d18dba5
Kate <katepdonahue@gmail.com>
2014-02-10 16:30:22 -0500

    Define users index route

--- angular_part/app/js/app.js
+++ angular_part/app/js/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-var friendLibApp = angular.module('friendLibApp', [
-  'ngRoute',
-  'friendLibControllers'
+var friendLibApp = angular.module("friendLibApp", [
+  "ngRoute",
+  "friendLibControllers"
   ]);
+
+friendLibApp.config([
+  "$routeProvider",
+  function($routeProvider) {
+    $routeProvider.
+    when("/users", {
+      templateUrl: "partials/users.html",
+      controller: "UserListCtrl"
+    });
+  }]);
