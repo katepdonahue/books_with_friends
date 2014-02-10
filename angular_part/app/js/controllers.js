5119689722d0146a53995c7a7fb65b30e963c9b1
Kate <katepdonahue@gmail.com>
2014-02-10 18:31:13 -0500

    Add controller for viewing one user

--- angular_part/app/js/controllers.js
+++ angular_part/app/js/controllers.js
@@ -6,10 +6,20 @@ friendLibControllers.controller('UserListCtrl', [
   '$scope',
   '$http',
   function($scope, $http) {
-    $http.get('localhost:3000/users').success(function(data) {
+    $http.get('/users.json').success(function(data) {
       $scope.users = data;
     });
 
     $scope.orderProp = 'name';
 
+  }]);
+
+friendLibControllers.controller('UserDataCtrl', [
+  '$scope',
+  '$http',
+  '$routeParams'
+  function($scope, $http, $routeParams) {
+    $http.get('/users/' + $routeParams.id + '.json').success(function(data) {
+      $scope.user = data[0];
+    });
   }]);
