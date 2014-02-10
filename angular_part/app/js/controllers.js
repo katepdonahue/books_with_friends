a5daab99b1eaa5238c5f799e5a2ef598b3831ab7
Kate <katepdonahue@gmail.com>
2014-02-10 16:40:35 -0500

    Add users route

--- angular_part/app/js/controllers.js
+++ angular_part/app/js/controllers.js
@@ -1,3 +1,15 @@
 'use strict';
 
 var friendLibControllers = angular.module('friendLibControllers', []);
+
+friendLibControllers.controller('UserListCtrl', [
+  '$scope',
+  '$http',
+  function($scope, $http) {
+    $http.get('localhost:3000/users').success(function(data) {
+      $scope.users = data;
+    });
+
+    $scope.orderProp = 'name';
+
+  }]);
