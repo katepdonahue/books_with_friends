f15b6e4463444b52306a1094325cdb0e2f2993f4
Kate <katepdonahue@gmail.com>
2014-02-12 15:17:32 -0500

    Get rid of get_info method and test

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,31 +7,5 @@ class Book < ActiveRecord::Base
   has_many :reviews
 
   accepts_nested_attributes_for :book_users
-
-  def get_info
-    hash = {}
-    hash[:id] = self.id
-    hash[:title] = self.title
-    hash[:genre] = self.genre
-    hash[:author] = "#{self.author.first_name} #{self.author.last_name}"
-    hash[:year] = self.year
-    hash[:isbn] = self.isbn
-    hash[:users] = []
-    self.users.each do |user|
-      hash[:users] << {:id => user.id, :username => user.username}
-    end
-    hash[:reviews] = []
-    self.reviews.each do |review|
-      hash[:reviews] << {:subject => review.subject,
-                         :body => review.body,
-                         :user_id => review.user_id,
-                         :username => review.user.username
-                        }
-    end
-    hash
-  end
-
-  def self.get_all_info(array = Book.all)
-  end
   
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,23 +5,6 @@ describe Book do
   describe "#get_info" do
     book = Book.find(1)
 
-    it "should know everything" do
-      hash = {
-        :id => 1,
-        :title => "The Best Book",
-        :genre => "happy",
-        :author => "Tillow Brown",
-        :year => 2009,
-        :isbn => "124562468",
-        :users => [{:id => 1, :username =>"Kate"}],
-        :reviews => [{:subject => "cool",
-                      :body => "It was the best book I've ever read.", 
-                      :user_id => 2,
-                      :username => "Jon"}]
-      }
-      expect(book.get_info).to eq(hash)
-    end
-
   end
 
 end
