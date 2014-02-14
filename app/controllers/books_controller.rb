37abaf3aeaa01c57bc84995bd5f3124ef24615c6
Kate <katepdonahue@gmail.com>
2014-02-14 15:06:03 -0500

    Move all that stuff into a book method in book model, call the method in create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,20 +23,7 @@ class BooksController < ActionController::Base
   def create
     @book = Book.new
     @book.isbn = params[:isbn]
-    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{@book.isbn}")
-    @book.title = response["items"][0]["volumeInfo"]["title"]
-    @book.genre = response["items"][0]["volumeInfo"]["categories"][0]
-    @book.year = response["items"][0]["volumeInfo"]["publishedDate"][0..3].to_i
-    @book.thumb = response["items"][0]["volumeInfo"]["imageLinks"]["smallThumbnail"]
-    @book.l_thumb = response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-    @book.description = response["items"][0]["volumeInfo"]["description"]
-    author = Author.new
-    name_array = response["items"][0]["volumeInfo"]["authors"][0].split
-    author.first_name = name_array[0..-2].join(" ")
-    author.last_name = name_array[-1]
-    author.save
-    @book.author_id = author.id
-    @book.save
+    @book.get_info
     # find author that matches name in database and grab author id, otherwise create new author
     redirect_to @book
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,5 +7,22 @@ class Book < ActiveRecord::Base
   has_many :reviews
 
   accepts_nested_attributes_for :book_users
+
+  def get_info
+    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{self.isbn}")
+    self.title = response["items"][0]["volumeInfo"]["title"]
+    self.genre = response["items"][0]["volumeInfo"]["categories"][0]
+    self.year = response["items"][0]["volumeInfo"]["publishedDate"][0..3].to_i
+    self.thumb = response["items"][0]["volumeInfo"]["imageLinks"]["smallThumbnail"]
+    self.l_thumb = response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+    self.description = response["items"][0]["volumeInfo"]["description"]
+    author = Author.new
+    name_array = response["items"][0]["volumeInfo"]["authors"][0].split
+    author.first_name = name_array[0..-2].join(" ")
+    author.last_name = name_array[-1]
+    author.save
+    self.author_id = author.id
+    self.save
+  end
   
 end
