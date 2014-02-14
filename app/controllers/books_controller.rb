672b6258f68b5bef6f42d2913a697b7c592a0d56
Kate <katepdonahue@gmail.com>
2014-02-14 15:21:16 -0500

    Update index action in books controller with new thumb column names

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,10 @@ class BooksController < ActionController::Base
   def index # users/:user_id/books or friends/books to see all of your friends books
     if params[:user_id]
       books = User.find(params[:user_id]).books
-      @books = books.as_json(:only => [:id, :title, :year, :l_thumb], :include => :author)
+      @books = books.as_json(:only => [:id, :title, :year, :thumb], :include => :author)
     else
       books = Book.all
-      @books = books.as_json(:only => [:id, :title, :year, :thumb], :include => [:author, :users => {:except => :email}])
+      @books = books.as_json(:only => [:id, :title, :year, :s_thumb], :include => [:author, :users => {:except => :email}])
     end
     respond_to do |format|
       format.json{ render :json => @books }
