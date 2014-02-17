d02ea64e0f3d58f725cc7209a70bbbeec6c94e09
Kate <katepdonahue@gmail.com>
2014-02-17 18:27:08 -0500

    Change to s_thumb even for users library so books.html will work for both

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ActionController::Base
   def index # users/:user_id/books or friends/books to see all of your friends books
     if user_signed_in?
       books = User.find(current_user.id).books
-      @books = books.as_json(:only => [:id, :title, :year, :thumb], :include => :author)
+      @books = books.as_json(:only => [:id, :title, :year, :s_thumb], :include => :author)
     else
       books = Book.all
       @books = books.as_json(:only => [:id, :title, :year, :s_thumb], :include => [:author, :users => {:except => :email}])
