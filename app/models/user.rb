b6dbb003d47963ea59d4cf729112c0a966453f7e
Kate <katepdonahue@gmail.com>
2014-02-17 18:02:42 -0500

    Add login stuff to angular index so it shows up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :username, :email
+  attr_accessible :username
 
   has_many :friendships
   has_many :friends, :through => :friendships
