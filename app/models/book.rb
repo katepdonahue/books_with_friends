66bcf4276cb940504750a7e248f4226bef20a261
Kate <katepdonahue@gmail.com>
2014-02-19 10:43:39 -0500

    Change root back to false cus it wasn't helping

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,8 +10,26 @@ class Book < ActiveRecord::Base
 
   accepts_nested_attributes_for :book_users
 
-  def get_info
-    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{self.isbn}")
+  # def get_info
+  #   response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{self.isbn}")
+  #   self.title = response["items"][0]["volumeInfo"]["title"]
+  #   self.genre = response["items"][0]["volumeInfo"]["categories"][0]
+  #   self.year = response["items"][0]["volumeInfo"]["publishedDate"][0..3].to_i
+  #   self.s_thumb = response["items"][0]["volumeInfo"]["imageLinks"]["smallThumbnail"]
+  #   self.thumb = response["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+  #   self.description = response["items"][0]["volumeInfo"]["description"]
+  #   author = Author.new
+  #   name_array = response["items"][0]["volumeInfo"]["authors"][0].split
+  #   author.first_name = name_array[0..-2].join(" ")
+  #   author.last_name = name_array[-1]
+  #   author.save
+  #   self.author_id = author.id
+  #   self.save
+  # end
+  
+
+  def custom_serialize
+    hash = {}
     self.title = response["items"][0]["volumeInfo"]["title"]
     self.genre = response["items"][0]["volumeInfo"]["categories"][0]
     self.year = response["items"][0]["volumeInfo"]["publishedDate"][0..3].to_i
@@ -26,5 +44,5 @@ class Book < ActiveRecord::Base
     self.author_id = author.id
     self.save
   end
-  
+
 end
