c59248c9df7dd15eb94b1630cb0ee96258a8ae91
Kate <katepdonahue@gmail.com>
2014-02-26 12:26:35 -0500

    Try to follow angular docs on directives :(

--- public/app/js/directives.js
+++ public/app/js/directives.js
@@ -1,8 +1,21 @@
 'use strict';
 
+angular.module('docsSimpleDirective', [])
+    .controller('Ctrl', function($scope) {
+      $scope.user = {
+        name: 'Naomi',
+        address: '1600 Amphitheatre'
+      };
+    })
+    .directive('myCustomer', function() {
+      return {
+        templateUrl: 'app/partials/current_user.html'
+      };
+    });
+
 angular.module('friendLibApp.directives', [])
        .directive('CurrentUser', function(injectables) {
-        return function(scope, elem, attrs) {
-          
-        }
+        return function(scope, element, attrs) {
+          console.log("am I doing it?");
+        };
        });
